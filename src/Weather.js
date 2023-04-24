a652b566a7fee566d296fe6b800605f103da2595
tinalou14 <117734587+tinalou14@users.noreply.github.com>
2023-04-24 15:39:50 +0100

    changed css on temp icon

--- src/Weather.js
+++ src/Weather.js
@@ -35,10 +35,10 @@ export default function Weather() {
             alt="Mostly Cloudy"
             className="float-left"
             />
-            <div className="float-left">
+            
             <span className="temperature">6</span>
             <span className="unit">C</span>
-              </div>
+              
             </div>
         </div>
         <div className="col-6">
