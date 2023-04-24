79c33eac82a6f4be2e7597f4fac24f7bb3ef9fd5
tinalou14 <117734587+tinalou14@users.noreply.github.com>
2023-04-24 11:34:32 +0100

    Added github link

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import react from "react";
 import './App.css';
 
 export default function App(){
-  return <div className="App">Weather App
-      
+  return <div className="App">
+    Weather App
+    <footer>
+      This project was coded by Tina Cooke and is {" "}
+      <a href="https://github.com/tinalou14/react-weather-app"
+      target="_blank"
+      >
+        open-sourced on GitHub
+      </a>
+      </footer>  
     </div>
 }
 
