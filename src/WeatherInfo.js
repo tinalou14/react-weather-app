8a3b46affc2871341f1f5a3842ee97b8b3e7d17d
tinalou14 <117734587+tinalou14@users.noreply.github.com>
2023-05-01 20:32:26 +0100

    changed img to canvas on weather css

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -16,7 +16,7 @@ export default function WeatherInfo(props) {
                 <div className="clearfix">
                     <div className="float-left">
                     <WeatherIcon 
-                    code={props.data.icon} />
+                    code={props.data.icon} /> 
                     </div>
             
             
