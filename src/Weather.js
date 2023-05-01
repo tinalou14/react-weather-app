b9e2650004d53188cbc7c021eda99564dd135d59
tinalou14 <117734587+tinalou14@users.noreply.github.com>
2023-05-01 19:43:47 +0100

    updated react added animated icon

--- src/Weather.js
+++ src/Weather.js
@@ -7,6 +7,7 @@ import "./Weather.css";
 export default function Weather(props) {
     const [weatherData, setWeatherData] =useState({ ready: false });
     const [city,setCity] = useState(props.defaultCity);
+    
     function handleResponse(response) {
         setWeatherData({
             ready: true,
@@ -14,7 +15,7 @@ export default function Weather(props) {
             humidity: response.data.main.humidity,
             date: new Date(response.data.dt * 1000),
             description: response.data.weather[0].description,
-            iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+            icon: response.data.weather[0].icon,
             wind: response.data.wind.speed,
             city: response.data.name
         });
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate"; 
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
     return (
@@ -13,11 +14,11 @@ export default function WeatherInfo(props) {
         <div className="row mt-3">
             <div className="col-6">
                 <div className="clearfix">
-            <img
-            src={props.data.iconUrl}
-            alt={props.data.description}
-            className="float-left"
-            />
+                    <div className="float-left">
+                    <WeatherIcon 
+                    code={props.data.icon} />
+                    </div>
+            
             
             <span className="temperature">{Math.round(props.data.temperature)}</span>
             <span className="unit">C</span>
