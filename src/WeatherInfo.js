f71b03a0aa24523755a50a01eea8412922495644
tinalou14 <117734587+tinalou14@users.noreply.github.com>
2023-05-08 22:26:32 +0100

    Created a weather temp change to fahrenheit

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate"; 
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
     return (
@@ -18,10 +19,8 @@ export default function WeatherInfo(props) {
                     <WeatherIcon code={props.data.icon} /> 
                     </div>
             
-            
-            <span className="temperature">{Math.round(props.data.temperature)}</span>
-            <span className="unit">°C</span>
-              
+            <WeatherTemperature celsius={props.data.temperature} />
+                          
             </div>
         </div>
         <div className="col-6">
